39b4521b1c0f99fb8f019dac7c66eabe275cd153
Jonathan Kidd <jonathan.kidd@uplight.com>
2021-05-25 15:59:38 -0400

    add datalabels and update chart config for version 2.9.3

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
-// import ChartDataLabels from 'chartjs-plugin-datalabels';
-// debugger
-// console.log(ChartDataLabels)
 d3.csv('https://s3-us-west-2.amazonaws.com/s.cdpn.io/2814973/atp_wta.csv')
-.then(makeChart);
+  .then(makeChart);
 
-function makeChart(players) {
-var chart = new Chart('chart', {
-  type: 'bar',
-  data: {
-    labels: ["February", "March", "April"],
-    datasets: [
-      {
-        label: "Last Year",
-        data: [1021, 1434, 1032],
-        backgroundColor: "#9FE0F8",
-        borderRadius: 5,
-        borderSkipped: false
-      },
-      {
-        label: "This Year",
-        data: [1121, 1734, 1692],
-        backgroundColor: "#093EDD",
-        borderRadius: 5,
-        borderSkipped: false
+Chart.plugins.register(ChartDataLabels);
 
-      }
-    ]
-  },
-  options: {
-    scales: {
-      y: {
-        display: false
-      },
-      x: {
-        display: false
-      },
+function makeChart() {
+  var chart = new Chart('chart', {
+    type: 'bar',
+    data: {
+      labels: ["February", "March", "April"],
+      datasets: [
+        {
+          label: "Last Year",
+          data: [1021, 1434, 1032],
+          backgroundColor: "#9FE0F8",
+          borderRadius: 5,
+          borderSkipped: false,
+          datalabels: {
+            align: 'end',
+            anchor: 'end'
+          }
+        },
+        {
+          label: "This Year",
+          data: [1121, 1734, 1692],
+          backgroundColor: "#093EDD",
+          borderRadius: 5,
+          borderSkipped: false,
+          datalabels: {
+            align: 'end',
+            anchor: 'end'
+          }
+        }
+      ]
     },
-    plugins: {
+    options: {
+      barRoundness: 0.5,
       legend: {
         display: false
       },
+      tooltips: {
+        enabled: false
+      },
+      scales: {
+        yAxes: [{
+          display: false,
+        }],
+        xAxes: [{
+          display: false,
+        }],
+      },
       plugins: {
         datalabels: {
-          color: 'blue',
-          labels: {
-            title: {
-              font: {
-                weight: 'bold'
-              }
-            },
-            value: {
-              color: 'green'
-            }
-          }
+          borderRadius: 4,
+          color: 'black',
+          font: {
+            weight: 'bold',
+            size: 25
+          },
+          // can use formatter to make any other adjustments to the label
+          formatter: function (value, context) {
+            return "$" + value;
+          },
+          padding: 6
         }
       }
     }
-  }
-});
+  });
 }
