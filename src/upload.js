0898629eb3bfd9ad6c01b7cf097747473968a066
Jonathan Kidd <jonathan.kidd@uplight.com>
2021-06-04 11:50:17 -0400

    add ignored file with keys, update readme

--- src/upload.js
+++ src/upload.js
@@ -1,15 +1,15 @@
 import domtoimage from 'dom-to-image';
 import S3 from 'react-aws-s3';
+import { awsKey, awsSAKey } from './awsKeys.js';
 
 export const uploadToS3 = (chart, accountNumber) => {
     let promise = domtoimage.toBlob(chart)
         .then(function (blob) {
-            // console.log(process.env)
             const config = {
                 bucketName: 'bengal-12-poc',
                 region: 'us-east-1',
-                // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                accessKeyId: awsKey,
+                secretAccessKey: awsSAKey,
                 s3Url: 'https://bengal-12-poc.s3.amazonaws.com'
             }
 
